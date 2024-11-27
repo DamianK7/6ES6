daec2a3848e56293023a57ce77f5011b5a41a69f
Damian <damiankontakt7@gmail.com>
2024-11-27 22:24:00 +0100

    Dodanie metody divide i ukonczenie programu

--- MathCalculator/MC.js
+++ MathCalculator/MC.js
@@ -14,8 +14,19 @@ class Calculator{
         console.log(`Wynik mnożenia: ${a} * ${b} = ${result}`);
     }
     
+    divide({a,b}){
+        if(b===0){
+            console.log("Nie można dzielić przez 0");
+            return;
+        }
+        let result = a / b;
+        console.log(`Wynik dzielenia: ${a} : ${b} = ${result}`);
+    }
 }
+
 const calculator = new Calculator();
+
 calculator.add({a : 5, b : 10});
 calculator.substruct({a : 9, b: 7});
-calculator.multiply({a : 6, b : 4})
+calculator.multiply({a : 6, b : 4});
+calculator.divide({a : 7, b : 2});
