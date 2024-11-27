daed67dc5342d500e4a234445046d940f38bb69b
Damian <gsega@gmail>
2024-11-27 15:05:50 +0100

    Zapis aplikacji z funkcjami uppercase i reverse

--- SimpleTextProcessor/STP.js
+++ SimpleTextProcessor/STP.js
@@ -1,1 +1,13 @@
- 
+const processText = (text, { uppercase = false, reverse = false, removeSpecialChars = false } = {}) =>{
+let processedText = text;
+
+if(uppercase){
+    processedText = processedText.toUpperCase();
+}
+if(reverse){
+    processedText = processedText.split('').reverse().join('')
+}
+
+console.log(`Zmieniony tekst: ${processedText}`)
+}
+processText("Witam", {uppercase:true, reverse:true})
