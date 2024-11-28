856ae075049ddfe36288147a4f1b956c37a3e9e6
Damian <damiankontakt7@gmail.com>
2024-11-28 20:11:07 +0100

    Dodanie metody getAverage i nowego objektu student2

--- StudentGradeManager/SGM.js
+++ StudentGradeManager/SGM.js
@@ -8,11 +8,20 @@ class Student{
     addGrade(...grades){
         this.grades = [...this.grades, ...grades];
     }
-    
+    getAverage(){
+        if(this.grades.length === 0){
+            return 0;
+        }
+        const sum = this.grades.reduce((total, grade) => total + grade, 0);
+        return (sum / this.grades.length).toFixed(2);
+    }
 }
 
 const student = new Student({name : "Marian Kot", age : 20});
+const student2 = new Student({name: "Tomasz Kowal", age : 23});
 
-student.addGrade(2,3,5,6);
+student.addGrade(2,3,5,6,3);
+student2.addGrade(5,3,6,4,5);
 
-console.log(`${student.name} ma oceny: ${student.grades}`)
+console.log(`${student.name} ma średnią ocen: ${student.getAverage()}`);
+console.log(`${student2.name} ma średnią ocen: ${student2.getAverage()}`);
