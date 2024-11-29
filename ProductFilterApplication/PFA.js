3ec0dd2eeab9597edf048943b4b0c64e1251f4f4
Damian <damiankontakt7@gmail.com>
2024-11-29 21:13:12 +0100

    Zastosowanie funkcji filterProducts

--- ProductFilterApplication/PFA.js
+++ ProductFilterApplication/PFA.js
@@ -1,8 +1,22 @@
 const products = [
     {name: "Laptop", price: 1200, category: "Electronics"},
     {name: "Sofa", price: 500, category: "Furniture"},
-    {name: "Fridger", price: 800, category: "Electronics"},
+    {name: "Fridge", price: 800, category: "Electronics"},
     {name: "Table", price: 250, category: "Furniture"},
     {name: "Smartphone", price: 900, category: "Electronics"}
 ];
 
+const filterProducts = (filters = {}) => {
+    const {category, minPrice = 0, maxPrice = Infinity} = filters;
+
+    const filtered = products.filter(({category : productCategory, price}) =>
+    (!category || productCategory === category) && price >= minPrice && price <= maxPrice);
+
+    filtered.forEach(({name,price,category}) => {
+        console.log(`Produkt: ${name}, Cena: ${price}, Kategoria: ${category}`);
+    });
+
+
+};
+
+filterProducts({category: "Electronics", maxPrice: 1000});
