b3528d7f60ccd730f37cd35f590f924f8d8b509d
Damian <damiankontakt7@gmail.com>
2024-11-29 19:33:34 +0100

    Dodanie tablicy obiektów - products

---
+++ ProductFilterApplication/PFA.js
@@ -0,0 +1,8 @@
+const products = [
+    {name: "Laptop", price: 1200, category: "Electronics"},
+    {name: "Sofa", price: 500, category: "Furniture"},
+    {name: "Fridger", price: 800, category: "Electronics"},
+    {name: "Table", price: 250, category: "Furniture"},
+    {name: "Smartphone", price: 900, category: "Electronics"}
+];
+
